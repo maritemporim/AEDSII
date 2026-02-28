dde991334b2575d171ff1e4bf254a1263f0f88b0
Mariana Temporim Ferreira <marianatemporim2007@gmail.com>
2026-02-28 20:03:33 -0300

    incio tp01

---
+++ 2026-1/TP/Palindromo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+public class Palindromo {
+    int sc = new Scanner(System.in);
+
+    String palavra = next.Line();
+    boolean jk = true;
+
+    while(palavra != "FIM"){
+        for(int i = 0; i < palavra.length(); i++){
+            
+        }
+    }
+
+    
+
+}
