e431d38c8871a74ab2f31dffa39c55db8c4595ea
Mariana Temporim Ferreira <marianatemporim2007@gmail.com>
2026-03-17 16:50:30 -0300

    inicio TP01

---
+++ 2026/TPs/TP01/Cesar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Cesar {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String palavra;
+        palavra = sc.nextLine();
+        String bts = "";
+        for(int i = 0; i < palavra.length(); i++){
+            char jk = palavra.charAt(i);
+            bts += (char) (jk + 3);
+        }
+
+        System.out.println(bts);
+        sc.close();
+    }
+}
